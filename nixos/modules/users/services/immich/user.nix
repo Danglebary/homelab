b17d42879ec489dbd2a27e0756e6f869ea159257
Austin See <halfblowncontact@gmail.com>
2025-09-05 14:16:18 -0700

    fix more config for immich

--- nixos/modules/users/services/immich/user.nix
+++ nixos/modules/users/services/immich/user.nix
@@ -17,7 +17,8 @@
   systemd.tmpfiles.rules = [
     # Service runtime directory (775 allows group write access for containers)
     "d /var/lib/services/immich 0775 immich services -"
-    "d /var/lib/services/immich/postgres 0775 immich services -"
+    # PostgreSQL directory needs postgres user ownership (UID 999)
+    "d /var/lib/services/immich/postgres 0750 999 999 -"
     "d /var/lib/services/immich/redis 0775 immich services -"
     "d /var/lib/services/immich/ml-cache 0775 immich services -"
 
