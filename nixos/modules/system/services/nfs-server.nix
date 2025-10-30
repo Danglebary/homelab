dd0f4f7ed31e26fdedb882d6fffa676d33a276f5
Austin See <halfblowncontact@gmail.com>
2025-10-30 15:35:35 -0700

    fix nfs anonuid

--- nixos/modules/system/services/nfs-server.nix
+++ nixos/modules/system/services/nfs-server.nix
@@ -6,7 +6,7 @@
 
     # Define exports
     services.nfs.server.exports = ''
-      /mnt/vault 192.168.68.0/24(rw,sync,no_subtree_check,all_squash,anonuid=1000,anongid=3000)
+      /mnt/vault 192.168.68.0/24(rw,sync,no_subtree_check,all_squash,anonuid=1001,anongid=3000)
     '';
 
     # Open firewall for NFS
