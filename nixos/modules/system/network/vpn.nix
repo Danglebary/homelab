5ea6d9280b566527b5def3f28c68cadf00d9d508
Austin See <halfblowncontact@gmail.com>
2026-01-07 23:03:46 -0800

    fix killswitch service

--- nixos/modules/system/network/vpn.nix
+++ nixos/modules/system/network/vpn.nix
@@ -90,29 +90,35 @@ in
             RemainAfterExit = true;
 
             ExecStart = pkgs.writeShellScript "vpn-killswitch" ''
-            ${nft} add table inet vpnkill || true
-            ${nft} flush table inet vpnkill
-            ${nft} add chain inet vpnkill output '{ type filter hook output priority -100; policy accept; }'
+            nft="${pkgs.nftables}/bin/nft"
 
-            ${lib.concatStringsSep "\n" (map (uid: ''
+            # Create table and chain
+            $nft add table inet vpnkill || true
+            $nft flush table inet vpnkill
+            $nft add chain inet vpnkill output '{ type filter hook output priority -100; policy accept; }'
+
+            # Runtime list of VPN users
+            for user in deluge sonarr radarr prowlarr; do
+                uid=$(id -u "$user" 2>/dev/null || continue)
+
                 # Allow loopback
-                ${nft} add rule inet vpnkill output meta skuid ${toString uid} oifname "lo" accept
+                $nft add rule inet vpnkill output meta skuid $uid oifname "lo" accept
 
                 # Block DNS outside tun0
-                ${nft} add rule inet vpnkill output meta skuid ${toString uid} ip protocol udp udp dport 53 oifname != "tun0" drop
-                ${nft} add rule inet vpnkill output meta skuid ${toString uid} ip protocol tcp tcp dport 53 oifname != "tun0" drop
+                $nft add rule inet vpnkill output meta skuid $uid ip protocol udp udp dport 53 oifname != "tun0" drop
+                $nft add rule inet vpnkill output meta skuid $uid ip protocol tcp tcp dport 53 oifname != "tun0" drop
 
                 # Drop all other traffic outside tun0
-                ${nft} add rule inet vpnkill output meta skuid ${toString uid} oifname != "tun0" drop
-            '') vpnUIDs)}
+                $nft add rule inet vpnkill output meta skuid $uid oifname != "tun0" drop
+            done
             '';
-
 
             ExecStop = ''
             ${pkgs.nftables}/bin/nft delete table inet vpnkill 2>/dev/null || true
             '';
         };
     };
+
 
     #### Optional but strongly recommended (prevent IPv6 leaks) ####
     networking.enableIPv6 = false;
